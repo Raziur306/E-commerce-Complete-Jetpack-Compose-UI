724b7d3faa66263c5b159cf37a18ea1d3f58ce70
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-01-20 16:34:04 +0600

    initial commit

---
+++ app/src/main/java/com/eritlab/jexmon/presentation/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.eritlab.jexmon.presentation.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
