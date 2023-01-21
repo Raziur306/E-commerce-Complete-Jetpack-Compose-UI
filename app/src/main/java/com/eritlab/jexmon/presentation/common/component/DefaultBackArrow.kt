3ef626b36858cf8dc54396e608cb8e79106a1ba2
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-01-21 15:33:15 +0600

    default back arrow btn

---
+++ app/src/main/java/com/eritlab/jexmon/presentation/common/component/DefaultBackArrow.kt
@@ -0,0 +1,36 @@
+package com.eritlab.jexmon.presentation.common.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.eritlab.jexmon.R
+
+@Composable
+fun DefaultBackArrow(
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .size(30.dp)
+            .clip(CircleShape)
+            .clickable {
+                onClick()
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.back_icon),
+            contentDescription = "Arrow Back",
+            modifier = Modifier.size(20.dp)
+        )
+    }
+}
